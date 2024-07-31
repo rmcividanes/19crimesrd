358f67d4619674c8912ec45235d195d8a8070c87
rmcividanes <rmcividanes1994@gmail.com>
2024-07-31 05:49:12 -0400

    Describe your changes here

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-// Resolve the current directory path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -13,13 +6,6 @@ const nextConfig = {
     NEXT_PUBLIC_EMAIL_SERVICE_ID: process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
     NEXT_PUBLIC_EMAIL_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
     NEXT_PUBLIC_EMAIL_USER_ID: process.env.NEXT_PUBLIC_EMAIL_USER_ID,
-  },
-  webpack(config) {
-    // Ensure Webpack can resolve paths
-    config.resolve.alias["@components"] = resolve(__dirname, "src/components");
-
-    // Add more Webpack configurations if needed
-    return config;
   },
 };
 
