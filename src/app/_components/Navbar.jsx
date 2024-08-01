125a10b26655d4876a876d677f99dc2fa739b40a
rmcividanes <rmcividanes1994@gmail.com>
2024-07-31 22:27:13 -0400

    Revised Edition

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -8,43 +8,38 @@ import Instagram from "../images/Instagram-icon.png";
 function Navbar() {
   return (
     <nav className="bg-black text-white flex px-4">
-      <div className="flex">
-        {" "}
-        <div className="p-4 w-1/4 focus-visible">
-          <Link href="/">SUBE TU FACTURA</Link>
-        </div>
-        <div className="p-4 w-1/4 ">
-          {" "}
-          <Link
-            href="Bases-Legales.pdf"
-            rel="noopener noreferrer"
-            target="_blank"
-            alt="alt text"
-          >
-            TÉRMINOS Y CONDICIONES
-          </Link>
-        </div>
+      {" "}
+      <div className="p-4">
+        <Link href="/">SUBE TU FACTURA</Link>
       </div>
-      <div className="flex w-2/3 py-4 justify-between">
+      <div className="p-4">
         {" "}
-        <div className="">
-          {" "}
-          <Link
-            href="https://www.facebook.com/share/UZnEf9pPe89kzia7/?mibextid=LQQJ4d"
-            target="_blank"
-          >
-            <Image src={Facebook} alt="facebook-link" className="w-10" />
-          </Link>
-        </div>
-        <div className="">
-          {" "}
-          <Link
-            href="https://www.instagram.com/19crimesrd?igsh=aTZvc3YxdjdkbjVr"
-            target="_blank"
-          >
-            <Image src={Instagram} alt="instagram-link" className="w-10" />
-          </Link>
-        </div>
+        <Link
+          href="Bases-Legales.pdf"
+          rel="noopener noreferrer"
+          target="_blank"
+          alt="alt text"
+        >
+          TÉRMINOS Y CONDICIONES
+        </Link>
+      </div>
+      <div className="py-4 mx-4">
+        {" "}
+        <Link
+          href="https://www.facebook.com/share/UZnEf9pPe89kzia7/?mibextid=LQQJ4d"
+          target="_blank"
+        >
+          <Image src={Facebook} alt="facebook-link" className="w-10" />
+        </Link>
+      </div>
+      <div className="py-4">
+        {" "}
+        <Link
+          href="https://www.instagram.com/19crimesrd?igsh=aTZvc3YxdjdkbjVr"
+          target="_blank"
+        >
+          <Image src={Instagram} alt="instagram-link" className="w-10" />
+        </Link>
       </div>
     </nav>
   );
